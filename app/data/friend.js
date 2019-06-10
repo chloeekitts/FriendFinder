1bd871195357fb24dded68794cd7dacb4899383e
Chloe Kitts <47789998+chloeekitts@users.noreply.github.com>
2019-06-10 15:15:04 -0500

    updating info

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,6 +1,7 @@
 var friendList = [
   {
-    name: 'Chloe Kitts',
+    name: 'Ahmed',
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores: [
       "4",
       "5",
