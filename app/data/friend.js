cfcab387dad9eead43bea3b00df617bf7c7c8d49
Chloe Kitts <47789998+chloeekitts@users.noreply.github.com>
2019-06-10 15:11:27 -0500

    Update friend.js

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,9 +1,6 @@
-//array that holds all the 'friends' that have taken the survey
-//initialize starting with 'dummy' data
 var friendList = [
   {
     name: 'Chloe Kitts',
-    profilePic: 'https://avatars1.githubusercontent.com/u/17751317?v=3&s=466',
     scores: [
       "4",
       "5",
